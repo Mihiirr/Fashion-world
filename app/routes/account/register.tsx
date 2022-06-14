298e89c87bdb2ebae1ae6c42d30482a700058e82
Mihir Patel <patelmihir5398@gmail.com>
2022-06-14 12:06:45 +0530

    Solved error while registering User

--- app/routes/account/register.tsx
+++ app/routes/account/register.tsx
@@ -1,5 +1,5 @@
-import type { ActionFunction } from "@remix-run/node";
-import { Form, Link, useActionData } from "@remix-run/react";
+import { ActionFunction, json, redirect } from "@remix-run/node";
+import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
 import React from "react";
 import Logo from "~/components/Logo";
 import { db } from "~/services/client.server";
@@ -19,6 +19,15 @@ function validatePassword(password: unknown) {
   }
 }
 
+function validateUrl(url: any) {
+  console.log(url);
+  let urls = ["/", "https://remix.run"];
+  if (urls.includes(url)) {
+    return url;
+  }
+  return "/";
+}
+
 type ActionData = {
   formError?: string;
   fieldErrors?: {
@@ -31,14 +40,21 @@ type ActionData = {
   };
 };
 
+const badRequest = (data: ActionData) => json(data, { status: 400 });
+
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData();
   const username = form.get("username");
   const password = form.get("password");
-  if (typeof username !== "string" || typeof password !== "string") {
-    return {
+  const redirectTo = validateUrl(form.get("redirectTo") || "/");
+  if (
+    typeof username !== "string" ||
+    typeof password !== "string" ||
+    typeof redirectTo !== "string"
+  ) {
+    return badRequest({
       formError: `Form not submitted correctly.`,
-    };
+    });
   }
 
   const fields = { username, password };
@@ -46,22 +62,25 @@ export const action: ActionFunction = async ({ request }) => {
     username: validateUsername(username),
     password: validatePassword(password),
   };
-  if (Object.values(fieldErrors).some(Boolean)) return { fieldErrors, fields };
+  if (Object.values(fieldErrors).some(Boolean))
+    return badRequest({ fieldErrors, fields });
+
   const userExists = await db.user.findFirst({
     where: { username },
   });
   if (userExists) {
-    return {
+    return badRequest({
       fields,
       formError: `User with Username: ${username} already exists`,
-    };
+    });
   }
   const user = await register(username, password);
-  return createUserSession(user.id);
+  return createUserSession(user.id, redirectTo);
 };
 
 const Register: React.FC<Props> = (props) => {
   const actionData = useActionData<ActionData>();
+  const [searchParams] = useSearchParams();
   return (
     <div>
       <div className="h-20 w-full mx-auto flex items-center justify-center border-b-2 mb-10">
@@ -72,39 +91,60 @@ const Register: React.FC<Props> = (props) => {
           <div className="flex items-center justify-center text-4xl mb-10">
             <p className="text-5xl">REGISTER</p>
           </div>
-          <Form
+          <form
             method="post"
             className="py-4 w-full h-auto flex flex-col items-center"
           >
+            <input
+              type="hidden"
+              name="redirectTo"
+              value={searchParams.get("redirectTo") ?? undefined}
+            />
             <div className="flex flex-col h-auto w-5/6 py-1 justify-between mb-6">
-              <label htmlFor="username" className="text-xl">
+              <label htmlFor="username-input" className="text-xl">
                 Username
               </label>
               <input
                 id="username-input"
                 name="username"
                 type="text"
+                defaultValue={actionData?.fields?.username}
+                aria-invalid={Boolean(actionData?.fieldErrors?.username)}
+                aria-errormessage={
+                  actionData?.fieldErrors?.username
+                    ? "username-error"
+                    : undefined
+                }
                 className="h-10 w-full border-2 px-4"
                 autoFocus
               />
               {actionData?.fieldErrors?.username ? (
-                <p className="text-red-500">
+                <p className="text-red-500" id="username-error" role="alert">
                   {actionData.fieldErrors.username}
                 </p>
               ) : null}
             </div>
             <div className="flex flex-col h-auto w-5/6 py-1 justify-between mb-6">
-              <label htmlFor="password" className="text-xl">
+              <label htmlFor="password-input" className="text-xl">
                 password
               </label>
               <input
                 id="password-input"
                 name="password"
                 type="password"
+                defaultValue={actionData?.fields?.password}
+                aria-invalid={
+                  Boolean(actionData?.fieldErrors?.password) || undefined
+                }
+                aria-errormessage={
+                  actionData?.fieldErrors?.password
+                    ? "password-error"
+                    : undefined
+                }
                 className="h-10 w-full border-2 px-4"
               />
               {actionData?.fieldErrors?.password ? (
-                <p className="text-red-500">
+                <p className="text-red-500" role="alert" id="password-error">
                   {actionData.fieldErrors.password}
                 </p>
               ) : null}
@@ -121,7 +161,7 @@ const Register: React.FC<Props> = (props) => {
                 {actionData.formError}, try different username!!!
               </p>
             ) : null}
-          </Form>
+          </form>
           <div className="flex items-center justify-center w-5/6">
             <p className="text-xl">
               Already have an account?{" "}
