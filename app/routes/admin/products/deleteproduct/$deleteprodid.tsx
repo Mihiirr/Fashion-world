afbf628da149dd31dc1d9b35a503e086d9450d33
Mihir Patel <patelmihir5398@gmail.com>
2022-06-22 11:49:41 +0530

    On deleting product, will delete image locally

--- app/routes/admin/products/deleteproduct/$deleteprodid.tsx
+++ app/routes/admin/products/deleteproduct/$deleteprodid.tsx
@@ -1,8 +1,20 @@
 import { LoaderFunction, redirect } from "@remix-run/node";
-import { deleteAProduct } from "~/services/queries/product.server";
+import { useLocation } from "@remix-run/react";
+import { deleteFileLocally } from "~/services/queries/imgupload.server";
+import {
+  deleteAProduct,
+  getUniqueProducts,
+} from "~/services/queries/product.server";
 
 export const loader: LoaderFunction = async ({ params }) => {
   const id = await params.deleteprodid;
   if (typeof id !== "string") return redirect(`/admin/products`);
+  const productDetails = await getUniqueProducts(id);
+  if (!productDetails) {
+    throw new Response("What a product! Not found.", {
+      status: 404,
+    });
+  }
+  await deleteFileLocally(productDetails.image);
   return deleteAProduct(id);
 };
--- app/services/queries/imgupload.server.ts
+++ app/services/queries/imgupload.server.ts
@@ -1,5 +1,6 @@
 import type { UploadHandler } from "@remix-run/node"; // or "@remix-run/cloudflare"
 import { unstable_createFileUploadHandler } from "@remix-run/node"; // or "@remix-run/cloudflare"
+import fs from "fs";
 
 export const standardFileUploadHandler = unstable_createFileUploadHandler({
   directory: "public/uploads",
@@ -7,4 +8,16 @@ export const standardFileUploadHandler = unstable_createFileUploadHandler({
 
 export const fileUploadHandler: UploadHandler = (args) => {
   return standardFileUploadHandler(args);
+};
+
+export const deleteFileLocally = (fileName: string) => {
+  const path = `public/uploads/${fileName}`;
+
+  try {
+    fs.unlinkSync(path);
+    console.log("File Removed");
+    //file removed
+  } catch (err) {
+    console.error(err);
+  }
 };
